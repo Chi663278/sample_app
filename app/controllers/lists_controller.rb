abc2a7800de91fe0a8057441c2f09c26bb712bf2
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-03-07 13:44:37 +0000

    [Update]3-8 Validation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,14 +6,13 @@ class ListsController < ApplicationController
 
   def create
     #1,2 データを受け取り、新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     #3 データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    #4 トップ画面へリダイレクト
-
-    # redirect_to 'top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    if @list.save
+      redirect_to list_path(@list.id) # true 詳細画面へリダイレクト
+    else
+      render :new # false 新規投稿ページを再表示
+    end
   end
 
   def index
